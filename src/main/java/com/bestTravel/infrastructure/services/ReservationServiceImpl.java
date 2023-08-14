9bcb3f5ed23092882d54f5304a181b99f7c9ed68
claudio.vilas <caitocd@gmail.com>
2023-08-14 15:35:20 -0300

    created crud reservations

---
+++ src/main/java/com/bestTravel/infrastructure/services/ReservationServiceImpl.java
@@ -0,0 +1,119 @@
+package com.bestTravel.infrastructure.services;
+
+import com.bestTravel.api.model.request.ReservationRequest;
+import com.bestTravel.api.model.response.HotelResponse;
+import com.bestTravel.api.model.response.ReservationResponse;
+import com.bestTravel.domain.entity.ReservationEntity;
+import com.bestTravel.domain.repository.CustomerRepository;
+import com.bestTravel.domain.repository.HotelRepository;
+import com.bestTravel.domain.repository.ReservationRepository;
+import com.bestTravel.infrastructure.abstract_services.ReservationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+/**
+ * @author claudio.vilas
+ * date: 08/2023
+ */
+
+@Service
+@Transactional
+@Slf4j
+@RequiredArgsConstructor
+public class ReservationServiceImpl implements ReservationService {
+    private final ReservationRepository reservationRepository;
+    private final HotelRepository hotelRepository;
+    private final CustomerRepository customerRepository;
+
+    private static final BigDecimal CHARGES_PRICE_PERCENTAGE = BigDecimal.valueOf(0.20);
+
+    @Override
+    public ReservationResponse create(ReservationRequest request) {
+        log.info("---> inicio servicio guardar reservacion");
+        log.info("---> buscando hotel id {}", request.getIdHotel());
+        var hotel = hotelRepository.findById(request.getIdHotel()).orElseThrow();
+        log.info("---> buscando cliente {}", request.getIdClient());
+        var customer = customerRepository.findById(request.getIdClient()).orElseThrow();
+        var reservation = ReservationEntity.builder()
+                .id(UUID.randomUUID())
+                .hotel(hotel)
+                .customer(customer)
+                .totalDays(request.getTotalDays())
+                .dateTimeReservation(LocalDateTime.now())
+                .dateStart(LocalDate.now())
+                .dateEnd(LocalDate.now().plusDays(request.getTotalDays()))
+                .price(hotel.getPrice().add(hotel.getPrice().multiply(CHARGES_PRICE_PERCENTAGE)))
+                .build();
+        log.info("---> guardando reservacion...");
+        var reservationNew = reservationRepository.save(reservation);
+        log.info("---> finalizado servicio guardar reservacion");
+        return this.mapToDto(reservationNew);
+    }
+
+    @Override
+    public ReservationResponse read(UUID uuid) {
+        log.info("---> inicio servcio buscar reservacion por id");
+        log.info("---> buscando reservacion id {}", uuid);
+        var reservation = reservationRepository.findById(uuid).orElseThrow();
+        log.info("---> finalizado servicio buscar reservacion por id");
+        return this.mapToDto(reservation);
+    }
+
+    @Override
+    public ReservationResponse update(ReservationRequest request, UUID uuid) {
+        log.info("---> inicio servicio actualizar reservacion");
+        log.info("---> buscando reservacion id {}", uuid);
+        var reservation = reservationRepository.findById(uuid).orElseThrow();
+        log.info("---> buscando hotel id {}", request.getIdHotel());
+        var  hotel = hotelRepository.findById(request.getIdHotel()).orElseThrow();
+        log.info("---> actualizando reservacion...");
+        reservation.setHotel(hotel);
+        reservation.setTotalDays(request.getTotalDays());
+        reservation.setDateTimeReservation(LocalDateTime.now());
+        reservation.setDateStart(LocalDate.now());
+        reservation.setDateEnd(LocalDate.now().plusDays(request.getTotalDays()));
+        reservationRepository.save(reservation);
+        reservation.setPrice(hotel.getPrice().add(hotel.getPrice().multiply(CHARGES_PRICE_PERCENTAGE)));
+        log.info("---> finalizado servicio actualizacion reservacion");
+        return this.mapToDto(reservation);
+    }
+
+    @Override
+    public void delete(UUID uuid) {
+        log.info("---> inicio servicio eliminar reservacion por id");
+        log.info("---> buscando reservacion id {}", uuid);
+        reservationRepository.findById(uuid).orElseThrow();
+        log.info("---> eliminando reservacion...");
+        reservationRepository.deleteById(uuid);
+        log.info("---> finalizado servicio eliminar por id");
+
+    }
+
+    @Override
+    public BigDecimal findPrice(Long idHotel) {
+        log.info("---> inicio servicio buscar precio reservacion");
+        log.info("---> buscando hotel {}", idHotel);
+        var hotel = hotelRepository.findById(idHotel).orElseThrow();
+        log.info("---> finalizado servicio buscar precio reservacion");
+        return hotel.getPrice().add(hotel.getPrice().multiply(CHARGES_PRICE_PERCENTAGE));
+    }
+
+    private ReservationResponse mapToDto(ReservationEntity reservation){
+        var response = new ReservationResponse();
+        BeanUtils.copyProperties(reservation, response);
+        var hotelResponse = new HotelResponse();
+        BeanUtils.copyProperties(reservation.getHotel(), hotelResponse);
+        response.setHotel(hotelResponse);
+        return response;
+    }
+
+
+}
--- src/main/java/com/bestTravel/infrastructure/services/TicketServiceImpl.java
+++ src/main/java/com/bestTravel/infrastructure/services/TicketServiceImpl.java
@@ -8,6 +8,7 @@ import com.bestTravel.domain.repository.CustomerRepository;
 import com.bestTravel.domain.repository.FlyRepository;
 import com.bestTravel.domain.repository.TicketRepository;
 import com.bestTravel.infrastructure.abstract_services.TicketService;
+import com.bestTravel.util.BestTravelUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
@@ -16,8 +17,12 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.UUID;
+
+/**
+ * @author claudio.vilas
+ * date: 08/2023
+ */
 
 @Transactional
 @Service
@@ -28,6 +33,8 @@ public class TicketServiceImpl implements TicketService {
     private final FlyRepository flyRepository;
     private final CustomerRepository customerRepository;
     private final TicketRepository ticketRepository;
+
+    private static final BigDecimal CHARGER_PRICE_PERCENTAGE = BigDecimal.valueOf(0.25);
 
     @Override
     public TicketResponse create(TicketRequest request) {
@@ -41,10 +48,10 @@ public class TicketServiceImpl implements TicketService {
                 .id(UUID.randomUUID())
                 .fly(fly)
                 .customer(customer)
-                .price(fly.getPrice().multiply(BigDecimal.valueOf(0.25)))
+                .price(fly.getPrice().add(fly.getPrice().multiply(CHARGER_PRICE_PERCENTAGE)))
                 .purchaseDate(LocalDate.now())
-                .arrivalDate(LocalDateTime.now())
-                .departureDate(LocalDateTime.now())
+                .arrivalDate(BestTravelUtil.getRandomLatter())
+                .departureDate(BestTravelUtil.getRandomSoom())
                 .build();
         var ticket = ticketRepository.save(ticketPersist);
         log.info("--> guardado ticket id {}", ticket.getId());
@@ -69,9 +76,9 @@ public class TicketServiceImpl implements TicketService {
         var fly = flyRepository.findById(request.getIdFly()).orElseThrow();
         log.info("---> actualizando ticket");
         tickeBase.setFly(fly);
-        tickeBase.setPrice(fly.getPrice().multiply(BigDecimal.valueOf(0.25)));
-        tickeBase.setDepartureDate(LocalDateTime.now());
-        tickeBase.setDepartureDate(LocalDateTime.now());
+        tickeBase.setPrice(fly.getPrice().add(fly.getPrice().multiply(CHARGER_PRICE_PERCENTAGE)));
+        tickeBase.setDepartureDate(BestTravelUtil.getRandomSoom());
+        tickeBase.setDepartureDate(BestTravelUtil.getRandomLatter());
         var ticketUpdate = ticketRepository.save(tickeBase);
         log.info("---> finalizado servicio actualizar ticket");
         return mapToDTO(ticketUpdate);
@@ -81,10 +88,19 @@ public class TicketServiceImpl implements TicketService {
     public void delete(UUID uuid) {
         log.info("---> inicio servicio eliminar ticket");
         log.info("---> buscando ticket con id {}", uuid);
-        var ticket = ticketRepository.findById(uuid).orElseThrow();
+        ticketRepository.findById(uuid).orElseThrow();
         log.info("---> eliminando ticket con id {}", uuid);
         ticketRepository.deleteById(uuid);
         log.info("---> finalizado servicio eliminar ticket");
+    }
+
+    @Override
+    public BigDecimal findPrice(Long idFly) {
+        log.info("---> inicio servicio obtener precio de ticket");
+        log.info("---> obteniendo vuelo...");
+        var fly = flyRepository.findById(idFly).orElseThrow();
+        log.info("---> finalizado servicio obtener precio de ticket");
+        return fly.getPrice().add(fly.getPrice().multiply(CHARGER_PRICE_PERCENTAGE));
     }
 
     private TicketResponse mapToDTO(TicketEntity entity){
@@ -95,4 +111,6 @@ public class TicketServiceImpl implements TicketService {
         response.setFly(flyResponse);
         return response;
     }
+
+
 }
